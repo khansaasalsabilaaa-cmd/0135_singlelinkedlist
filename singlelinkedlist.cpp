c3ca91271f4df6022e7aea6ceaf1897fe5c0a0ae
khansaasasabilaaa-eng <khansaasasabilaaa@gmail.com>
2026-04-29 17:54:01 +0700

    menambahkan case

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -105,7 +105,7 @@ public:
             cout << "\ndata di dalam list adalah:\n";
             Node*currentNode = START;
 
-            while (currentNode ! = NULL)
+            while (currentNode != NULL)
             {
                 cout << currentNode->noMhs << endl;
                 currentNode = currentNode->next;
@@ -113,5 +113,80 @@ public:
            cout << endl;
         }
     } 
-}
+};
 
+int main()
+{
+    LinkedList mhs;
+    int nim;
+    char ch;
+
+    do
+    {
+        cout << "1.Menambah data ke dalam list" << endl;
+        cout << "2.Menghapus data dari dalam list" << endl;
+        cout << "3.Menampilkan semua data didalam list" << endl;
+        cout << "4.Mencari data dalam list" << endl;
+        cout << "5.Keluar" << endl;
+
+        cout << "\nMasukkan pilihan (1-5) : ";
+        cin >> ch;
+
+        switch (ch)
+        {
+        case '1':
+            mhs.addNode();
+            break;
+
+        case '2':
+            if (mhs.listEmpty())
+            {
+                cout << "\nList Kosong" << endl;
+                break;
+            }
+
+            cout << "\nMasukkan no mahasiswa yang akan dihapus : ";
+            cin >> nim;
+
+            if (mhs.delNode(nim) == false)
+                cout << "\nData tidak ditemukan" << endl;
+            else
+                cout << "\nData dengan nomor mahasiswa " << nim << "berhasil dihapus\n";
+            break;
+        
+        case '3':
+            mhs.traverse();
+            break;
+
+        case '4':
+        {
+            if (mhs.listEmpty())
+            {
+                cout << "\nList Kosong\n";
+                break;
+            }
+
+            Node *previous, *current;
+            cout << "\nMasukkan no mahasiswa yang dicari : ";
+            cin >> nim;
+
+            if (mhs.search(nim, previous, current) == false)
+                cout << "\nData tidak ditemukan\n";
+            else
+            {
+                cout << "\nData ditemukan\n";
+                cout << "NIM Mahasiswa : " << current->noMhs << endl;
+            }
+            break;
+        }
+
+        case '5':
+            break;
+
+        default:
+            cout << "\nPilihan salah\n";
+        }
+        
+    } 
+    
+}
