2eb4314559aaa8f0b668177895a327df5a8ad8ba
khansaasasabilaaa-eng <khansaasasabilaaa@gmail.com>
2026-04-29 17:38:31 +0700

    menambahkan traverse

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -88,10 +88,30 @@ public:
         if (current == START)
             START = START->next;
         else
-            previous->next = current->;
+            previous->next = current->next;
         
         delete current;
         return true;
     }
-    
+
+    void traverse()
+    {
+        if (listEmpty())
+        {
+            cout << "\nList Kosong\n";
+        }
+        else
+        {
+            cout << "\ndata di dalam list adalah:\n";
+            Node*currentNode = START;
+
+            while (currentNode ! = NULL)
+            {
+                cout << currentNode->noMhs << endl;
+                currentNode = currentNode->next;
+            }
+           cout << endl;
+        }
+    } 
 }
+
