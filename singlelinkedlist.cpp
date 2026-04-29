b4d6bcf848df88b464ca51948fb3d01deec83d84
khansaasasabilaaa-eng <khansaasasabilaaa@gmail.com>
2026-04-29 17:20:12 +0700

    menambahkan search

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -64,5 +64,16 @@ public:
         return (START == NULL);
     }
 
-    
+    bool search(int nim, Node *&previous, Node *&current)
+    {
+        previous = START;
+        current = START;
+
+        while (current != NULL && nim != current->noMhs)
+        {
+            previous = current;
+            current = current->next;
+        }
+        
+    }
 }
